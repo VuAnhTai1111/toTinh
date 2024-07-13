e79de1feaaaaabf64315ec64eaaeb4995d616484
VuAnhTai1111 <vuanhtai06011979@gmail.com>
2024-07-13 23:13:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Cô gái anh yêu à!!!',
+    introDesc: `Anh cũng có thể che giấu đi tất cả nhưng không thể nào giấu được điều là anh thương em.Ngay bây giờ và mãi mãi anh vẫn sẽ luôn dành trọn 1 tình yêu chân thành của mình cho em.`,
+    btnIntro: 'em bấm nút này đi',
+    title: 'Anh quá yêu em rồi nên anh có thể sẵn sàng chờ em  😙',
+    desc: 'Mặc dù em đã 2 lần từ chối anh.Anh thực sự rất là buồn nhưng anh tin là trong tim em cũng có hình bóng của anh đúng không ',
+    btnYes: 'Vâng!! Thực ra em cũng rất thích anh',
+    btnNo: 'Không,Anh cứ tiếp tục mơ đi:3',
+    question:'Vậy em cho anh cơ hội thêm lần nữa nhé và đây là lần thứ 3 anh tỏ tình em . Em sẵn sàng chưa <3',
+    btnReply: 'Tiếp đi anh <3',
+    reply: 'Em đang rất trông chờ nè <33333333',
+    mess: 'Anh biết em sẽ trả lời vậy mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'vậy thì hãy xem video này thay cho lời tỏ tình của anh nha',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.youtube.com/watch?v=q32vSbNdiGw' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
